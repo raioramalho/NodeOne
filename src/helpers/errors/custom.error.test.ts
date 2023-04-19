c759e1b655ee22dd59d21f9254d2c62a95c184e1
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-19 20:31:28 -0300

    testes unitarios dos helpers.

---
+++ src/helpers/errors/custom.error.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, expectTypeOf, test } from 'vitest';
+import { CustomError } from './custom.error';
+
+
+describe('Bateria de testes relacionadas a custom error', () => {
+	test('Deve verificar a classe custom error', () => {
+		const test1: CustomError = {
+			code: 404,
+			name: 'NotFound',
+			message: 'Teste'
+		};
+		expectTypeOf(test1).toBeObject();
+	});
+});
--- src/helpers/tools/hash.tool.test.ts
+++ src/helpers/tools/hash.tool.test.ts
@@ -1,7 +1,9 @@
 import { describe, expect, test } from 'vitest';
+import { hashHelper } from './hash.tool';
 describe('Bateria de testes para o hashtool', () => {
-	test('test', () => {
+	test('test', async () => {
 		const clean_password = '1q2w3e';
-		expect(clean_password).toBe('1q2w3e');
+		const hash_password = await hashHelper.generate(clean_password);
+		expect(await hashHelper.compare(clean_password,hash_password)).toBe(true);
 	});
 });
