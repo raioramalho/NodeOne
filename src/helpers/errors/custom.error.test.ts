151f5e7328efc19dbf37e5c3f56aa5febfdda9d2
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-21 01:32:15 -0300

    remove lines & format

--- src/helpers/errors/custom.error.test.ts
+++ src/helpers/errors/custom.error.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, expectTypeOf, test } from 'vitest';
 import { CustomError } from './custom.error';
 
-
 describe('Bateria de testes relacionadas a custom error', () => {
 	test('Deve verificar a classe custom error', () => {
 		const test1: CustomError = {
