b3e104e9624cc0d5f6794ebb6fdc52c766b6afad
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-21 01:26:42 -0300

    edit items

--- src/helpers/tools/hash.tool.test.ts
+++ src/helpers/tools/hash.tool.test.ts
@@ -5,6 +5,6 @@ describe('Bateria de testes para o hashtool', () => {
 	test('test', async () => {
 		const clean_password = '1q2w3e';
 		const hash_password = await hashHelper.generate(clean_password);
-		expect(await hashHelper.compare(clean_password,hash_password)).toBe(true);
+		expect(await hashHelper.compare(clean_password, hash_password)).toBe(true);
 	});
 });
