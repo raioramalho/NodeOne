ce18e34d3f3a37db735aaa2147377539ea9cc119
Alan Ramalho <ramalho.sit@gmail.com>
2023-04-23 21:02:46 -0300

    fixing, test

--- src/helpers/errors/custom.error.test.ts
+++ src/helpers/errors/custom.error.test.ts
@@ -1,6 +1,10 @@
-import { describe, expect, expectTypeOf, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
+import { ResponseError } from './response.error';
+import { errorList } from './list.error';
 
 describe('Bateria de testes relacionadas a custom error', () => {
 	test('Deve verificar a classe custom error', () => {
+		const customError = new ResponseError(errorList.Not_Found_Error);
+		expect(customError.code).toBe(404);
 	});
 });
