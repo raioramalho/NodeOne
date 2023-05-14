8da26b3d58d241e70f609755bd6f39f63a906f02
Alan Ramalho <ramalho.sit@gmail.com>
2023-05-14 15:49:23 -0300

    Typing error list

--- src/helpers/errors/list.error.ts
+++ src/helpers/errors/list.error.ts
@@ -1,20 +1,31 @@
 import { HttpStatus } from '@nestjs/common';
 
+interface IError {
+	name: string;
+	code: number;
+	message: string;
+}
+
 class ErrorList {
-	Internal_Server_Error = {
+	Internal_Server_Error: IError = {
 		name: 'Internal_Server_Error',
 		code: HttpStatus.INTERNAL_SERVER_ERROR,
 		message: 'Internal server error!'
 	};
-	Already_Exists = {
+	Already_Exists: IError = {
 		name: 'Already_Exists_Error',
 		code: HttpStatus.CONFLICT,
 		message: 'This is not avaliable!'
 	};
-	Not_Found_Error = {
+	Not_Found_Error: IError = {
 		name: 'Not_Found_Error',
 		code: HttpStatus.NOT_FOUND,
 		message: 'No data found!'
+	};
+	Bad_Request_Error: IError = {
+		name: 'Bad_Request_Error',
+		code: HttpStatus.BAD_REQUEST,
+		message: 'Bad request input!'
 	};
 }
 
