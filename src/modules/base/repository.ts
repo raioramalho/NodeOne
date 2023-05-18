904f22f04c776d2a01586029ba1b159a89e9570f
Alan Ramalho <ramalho.sit@gmail.com>
2023-05-18 19:26:28 -0300

    new method, more generic.

--- src/modules/base/repository.ts
+++ src/modules/base/repository.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 //adicione seus models aqui!
-type ModelName = 'Example' | 'ModelTwo'
+type ModelName = Prisma.ModelName;
 
 export class Repository{
 	constructor(public readonly model: ModelName) {}
@@ -11,71 +11,99 @@ export class Repository{
 	async findMany(includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].findMany({
+		const result = await prisma[this.model.toLocaleLowerCase()].findMany({
 			include: includes,
 		});
+		prisma.$disconnect();
+		return result;
 	}
 
 	async findById(id: number, includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].findUnique({
+		const result = await prisma[this.model.toLocaleLowerCase()].findUnique({
 			where: { id },
 			include: includes,
 		});
+		prisma.$disconnect();
+		return result;
+	}
+
+	async findByObj(obj: object, includes?: object) {
+		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		//@ts-ignore
+		const result = await prisma[this.model.toLocaleLowerCase()].findUnique({
+			where: obj,
+			include: includes,
+		});
+		prisma.$disconnect();
+		return result;
 	}
 
 	async findByEmail(email: string, includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].findUnique({
+		const result = await prisma[this.model.toLocaleLowerCase()].findUnique({
 			where: { email: email },
 			include: includes,
 		});
+		prisma.$disconnect();
+		return result;
 
 	}
 
 	async updateById(id: number, data: any, includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].update({
+		const result = await prisma[this.model.toLocaleLowerCase()].update({
 			where: { id },
 			data,
 			includes,
 		});
+		prisma.$disconnect();
+		return result;
 	}
 
 	async updateByEmail(email: string, data: any, includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].update({
+		const result = await prisma[this.model.toLocaleLowerCase()].update({
 			where: { email },
 			data,
 			includes,
 		});
+		prisma.$disconnect();
+		return result;
 	}
 
 	async deleteById(id: number, includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].delete({
+		const result = await prisma[this.model.toLocaleLowerCase()].delete({
 			where: { id },
 		});
+		prisma.$disconnect();
+		return result;
 	}
 
 	async deleteByEmail(email: string, includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].delete({
+		const result = await prisma[this.model.toLocaleLowerCase()].delete({
 			where: { email },
 		});
+		prisma.$disconnect();
+		return result;
 	}
 
 	async create(data: any, includes?: object) {
 		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].create({
+		const result = await prisma[this.model.toLocaleLowerCase()].create({
 			data,
+			includes,
 		});
+		prisma.$disconnect();
+		return result;
 	}
 }
--- src/modules/example/example.repository.ts
+++ src/modules/example/example.repository.ts
@@ -3,10 +3,4 @@ import { Repository } from '../base/repository';
 
 const prisma = new PrismaClient();
 
-export class ExampleRepository extends Repository {
-	async deleteMany() {
-		//eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		//@ts-ignore
-		return await prisma[this.model.toLocaleLowerCase()].deleteMany();
-	}
-}
+export class ExampleRepository extends Repository {}
